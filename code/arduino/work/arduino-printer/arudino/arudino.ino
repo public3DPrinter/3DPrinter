e749e53f5f724981d97cf64c5a5f0d5715885366
TangLin <john-200000000@163.com>
2013-08-12 20:51:38 +0800

    Initial controll part for the arduino and make it able to controll stepper

---
+++ code/arduino/work/arduino-printer/arudino/arudino.ino
@@ -0,0 +1,59 @@
+#include <Stepper.h>
+
+#include "controller.h"
+
+Stepper stepper(DEFAULT_STEPS,LEFT1,LEFT2,RIGHT1,RIGHT2);
+
+void setup(){
+  setupStepper();
+  Serial.begin(PORT_RATE);
+}
+
+void loop(){
+  char _RECV[SIZE];
+  recv_data(_RECV);
+  state = state_machine(_RECV);
+  controlServo(state);
+  delay(100);
+}
+
+void setupStepper(){
+  stepper.setSpeed(DEFAULT_SPEED);		
+}
+
+
+void forward(double range){
+  stepper.step(range);
+  Serial.print("F");
+}
+
+void backward(double range){
+  stepper.step(-range);
+  Serial.print("F");
+}
+
+void recv_data(char* _RECV){
+  int counter=0;
+  memset(_RECV,'\0',SIZE);
+  if(Serial.available()){
+    _RECV[counter] = (char)Serial.read();
+    Serial.println(_RECV);
+  }
+}
+
+void controlServo(int state){
+    double range=0.0;
+    switch(state){
+      case 0:forward(B_RANGE);
+             break;
+      case 1:backward(N_RANGE);
+             break;
+      case 9:
+             break;
+    }
+}
+
+
+
+
+
---
+++ code/arduino/work/arduino-printer/arudino/controller.h
@@ -0,0 +1,35 @@
+#ifndef CONTROLLER_H_
+#define CONTROLLER_H_
+#include<string.h>
+#define SIZE 16
+#define PORT_RATE 19200
+#define LEFT1 6
+#define LEFT2 7
+#define RIGHT1 8
+#define RIGHT2 9
+#define DEFAULT_STEPS 200
+#define DEFAULT_SPEED 60
+#define B_RANGE 800
+#define N_RANGE 400
+int state;
+void setupStepper();
+void recv_data(char* _RECV);
+int state_machine(char* _RECV);
+void controlServo(int state);
+void forward(double range);
+void backward(double range);
+
+int state_machine(char* _RECV){
+    if(!strcmp(_RECV,"B"))
+        return 0;
+    else if(!strcmp(_RECV,"N"))
+        return 1;
+    else
+        return 9;
+}
+
+
+
+
+
+#endif
